8df00e007cdfa418c9c00a47ab0abd3b3f875c2d
ruisheng <ruishengleen@gmail.com>
2015-06-16 11:09:50 +0800

    admin site

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Choice, Question
+
+# 嵌入问题去
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline] #答案
+    list_display = ('question_text', 'pub_date')
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
